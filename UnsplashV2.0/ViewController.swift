11146ac6a1c4f1e63e31e99523535f071e378ce8
Nik <nikitosvanilla@gmail.com>
2020-01-03 09:20:28 +0400

    Initial Commit

---
+++ UnsplashV2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UnsplashV2.0
+//
+//  Created by Nikita on 03/01/2020.
+//  Copyright © 2020 Nikita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
